7ab927e015ba9ab9555e649bf8c2aad60e74dd6c
Renato Ayau <renato.ayau@tio-gt.com>
2021-07-11 19:40:02 -0600

    feat: integrate dev and production webpack/react

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,26 +1,30 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
+    entry: path.resolve(__dirname, "../src/index.js"),
+    resolve: {
+        extensions: [".js", ".jsx", ".json"],
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
+                use: "babel-loader",
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                type: "asset",
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
             },
         ],
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, "../public/index.html"),
-            filename: "./index.html",
         }),
     ],
 };
